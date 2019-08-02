666bcd70f30587dc7fa19589f1d0abbe0fa58640
Evan Moon <bboydart91@gmail.com>
2019-08-02 16:20:33 +0900

    singleQuestion 함수 생성

--- utils.js
+++ utils.js
@@ -4,22 +4,38 @@ const r = readline.createInterface({
   output: process.stdout,
 });
 
-exports.getPlayerCount = function () {
+function singleQuestion (question, errorMsg = 'Readline error') {
   return new Promise((resolve, reject) => {
-    r.question('몇 명의 인원이 참여하시나요?(숫자만 입력해주세요!) ', answer => {
+    r.question(`${question} `, answer => {
       if (answer && typeof answer === 'string') {
-        answer = parseInt(answer);
         resolve(answer);
-        r.close();
       }
       else {
-        console.error('인원을 입력해주세요');
-        reject();
-        r.close();
+        reject(errorMsg);
       }
+      r.close();
     });
   });
+}
+
+exports.getPlayerCount = async function () {
+  try {
+    const playerCount = await singleQuestion('몇 명의 인원이 참여하시나요?(숫자만 입력해주세요!)');
+    return parseInt(playerCount);
+  }
+  catch (e) {
+    throw new Error(e);
+  }
 };
+
+exports.getPlayerNames = function (playerCount) {
+  try {
+    
+  }
+  catch (e) {
+    throw new Error(e);
+  }
+}
 
 exports.getRandomCount = function (cap = 1) {
   return Math.floor(Math.random() * (cap - 1)) + 1;
